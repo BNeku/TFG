ed4c66f05bf789faaf4c24b2fdef9f682dd36774
merchf <mercherr@ucm.es>
2020-10-13 14:02:47 +0200

    Interfaz principal con la funcion de entrenar red

---
+++ src/entrenamientoRed.m
@@ -0,0 +1,23 @@
+load HumanActivityTrain
+
+numFeatures = 3;
+numHiddenUnits = 200;
+numClasses = 5;
+
+layers = [ ...
+    sequenceInputLayer(numFeatures)
+    lstmLayer(numHiddenUnits,'OutputMode','sequence')
+    fullyConnectedLayer(numClasses)
+    softmaxLayer
+    classificationLayer];
+
+options = trainingOptions('adam', ...
+    'MaxEpochs',60, ...
+    'GradientThreshold',2, ...
+    'Verbose',0, ...
+    'Plots','training-progress');
+
+
+net = trainNetwork(XTrain,YTrain,layers,options);
+
+save(fullfile(pwd, 'src','red','netLSTMTFG'), 'net');
